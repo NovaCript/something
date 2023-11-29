c91d5d49b9b0731bb9a0d559f871b9c8ff67cbe6
Gleb Iatsenko <glebka.yatsenko@gmail.com>
2023-11-29 18:19:56 +0300

    add: models.py method get_paginate, create parser args page

---
+++ app/parser.py
@@ -0,0 +1,7 @@
+from flask_restx import reqparse
+
+parser = reqparse.RequestParser()
+parser.add_argument('page', type=int, default=1, help='Page number')
+parser.add_argument('per_page', type=int, default=10, help='Items per page')
+
+
